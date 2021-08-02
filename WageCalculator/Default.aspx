7ae65e5a0286b141b51a40cd960883b1b1f907c5
BlackHawk0 <danielngoys@outlook.com>
2021-08-02 11:09:52 +0300

    first commit

---
+++ WageCalculator/Default.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WageCalculator._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div class="jumbotron">
+        <p class="lead">Enter the employee id </p>
+        <p class="lead">&nbsp;</p>
+        <p class="lead">
+            <asp:TextBox ID="txtID" runat="server"></asp:TextBox>
+        </p>
+        <p>    <p class="lead">Enter the no of hours worked </p></p>
+        <p class="lead">
+            <asp:TextBox ID="txtHoursWorked" runat="server"></asp:TextBox>
+        </p>
+
+        <p>                <asp:Button ID="Button1" runat="server" Text="Calculate Wage" OnClick="Button1_Click" />
+            </p>
+    </div>
+
+    <div class="row">
+        <div class="col-md-4">
+            <p>
+                <asp:Label ID="lblWage" runat="server" Text="lblWage"></asp:Label>
+            </p>
+        </div>
+        <div class="col-md-4">
+            
+        </div>
+        <div class="col-md-4">
+        </div>
+    </div>
+
+</asp:Content>
---
+++ WageCalculator/Default.aspx.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WageCalculator
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            double employeewage;
+            Employee emp = new Employee();
+        emp.EmployeeID = Convert.ToInt32(txtID.Text);
+            emp.HourWorked = Convert.ToDouble(txtHoursWorked.Text);
+            emp.RatePerHour = 12;
+            lblWage.Text = Convert.ToString(emp.CalculateWage(emp.EmployeeID,emp.HourWorked));
+            
+                
+                }
+    }
+
+    public class Employee
+    {
+        int employeeID;
+        string employeeName;
+        double rateperhour;
+        double hourWorked;
+
+
+        public double HourWorked
+        {
+            get
+            {
+                return hourWorked;
+            }
+            set
+            {
+                hourWorked = value;
+            }
+        }
+
+
+
+
+
+
+        public double RatePerHour
+        {
+            get
+            {
+                return rateperhour;
+            }
+            set
+            {
+                rateperhour = value;
+            }
+        }
+
+        public int EmployeeID
+        {
+            get
+            {
+                return employeeID;
+            }
+            set
+            {
+                employeeID = value;
+            }
+        }
+
+
+
+
+
+        public double CalculateWage(int emploeeID,double hourWorked)
+        {
+          
+
+
+            double wage=0.0;
+
+            wage = hourWorked * RatePerHour;
+
+
+            return wage;
+
+        }
+
+    }
+    
+}
